932bc97e27c33af891aae7b4ca785ecea1ef24c9
JuanBaut <deserd@protonmail.com>
2023-08-15 19:47:01 -0500

    About remade with MUI

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -1,44 +1,61 @@
-import style from './About.module.css';
 import logo from '../../assets/meAbout.png';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Container,
+  IconButton,
+  Typography,
+} from '@mui/material';
 
 const About = () => {
   return (
-    <div className={style.container}>
-      <div className={style.main}>
-        <img className={style.image} src={logo} alt="ramlogo" />
-        <div className={style.tagsContainer}>
-          <p className={style.tagInfo}>
-            Hi! My name is Juan and I made this project to showcase my abilities
-            based in React and JS. If you would like to see more details, check
-            the Github or LinkedIn.
-          </p>
-          <p className={style.tags}>Origin: Bogota, Colombia</p>
-          <p className={style.tags}>Name: Juan Bautista</p>
-          <div className={style.iconContainer}>
-            <button className={style.button}>
-              <a
-                href="https://www.linkedin.com/in/juanandresbautistafsd/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <LinkedInIcon sx={{ color: 'azure', fontSize: 40 }} />
-              </a>
-            </button>
-            <button className={style.button}>
-              <a
-                href="https://github.com/JuanBaut"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GitHubIcon sx={{ color: 'azure', fontSize: 36 }} />
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container maxWidth="sm">
+      <Card>
+        <CardHeader
+          title="Juan Andres Bautista"
+          subheader="Full-stack developer"
+        />
+        <CardMedia
+          height="400"
+          component="img"
+          image={logo}
+          alt="Juan Andres"
+        />
+        <CardContent>
+          <Typography>
+            Hi! I made this project to showcase my abilities in React,
+            JavaScript and CSS libraries like Material UI. If you would like to
+            see more details, check the Github or LinkedIn.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            href="https://www.linkedin.com/in/juanandresbautistafsd/"
+            target="_blank"
+            rel="noopener noreferrer"
+            startIcon={<LinkedInIcon />}
+            variant="outlined"
+          >
+            LinkedIn
+          </Button>
+          <Button
+            href="https://github.com/JuanBaut"
+            target="_blank"
+            rel="noopener noreferrer"
+            startIcon={<GitHubIcon />}
+            variant="outlined"
+          >
+            Github
+          </Button>
+        </CardActions>
+      </Card>
+    </Container>
   );
 };
 
